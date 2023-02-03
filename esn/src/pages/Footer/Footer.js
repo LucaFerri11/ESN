7771a8121a3c5e8055c991c86834fac03e448ffc
Luca Ferri <ferriluca11@gmail.com>
2023-02-03 22:10:50 +0100

    logo slider with some bugs

--- esn/src/pages/Footer/Footer.js
+++ esn/src/pages/Footer/Footer.js
@@ -15,55 +15,57 @@ function Footer() {
 
   return (
     <div className="grid-container">
-      <div className="social">
-        <Tooltip title={facebookTooltip} arrow placement="top">
-          <a
-            href={"https://www.facebook.com/groups/1384367125377692"}
-            target="_blank"
-            className="linkFooter"
-          >
-            <BsFacebook />
-          </a>
-        </Tooltip>
-        <Tooltip title={instagramTooltip} arrow placement="top">
-          <a
-            href={"https://www.instagram.com/esncoruna/"}
-            target="_blank"
-            className="linkFooter"
-          >
-            <BsInstagram />
-          </a>
-        </Tooltip>
-        <Tooltip title={mapsTooltip} arrow placement="top">
-          <a
-            href={
-              "https://www.google.com/maps/place/Casa+do+Lagar+-+UDC/@43.332124,-8.412034,14z/data=!4m5!3m4!1s0x0:0xa2fa446abee0da28!8m2!3d43.3321242!4d-8.412034?hl=en"
-            }
-            target="_blank"
-            className="linkFooter"
-          >
-            <FiMapPin />
-          </a>
-        </Tooltip>
-        <Tooltip title={whatsappTooltip} arrow placement="top">
-          <a
-            // href={""}
-            target="_blank"
-            className="linkFooter"
-          >
-            <BsWhatsapp />
-          </a>
-        </Tooltip>
-      </div>
-      <div className="disclaimer">
-        &#169; ESN Coruña - &nbsp;
-        <Link to={"/privacy"} className="linkFooter">
-          Privacy
-        </Link>
-        &nbsp; - &nbsp;
-        <Link to={"/contact"} className="linkFooter">
-          Contact Us
-        </Link>
+      <div className="left">
+        <div className="social">
+          <Tooltip title={facebookTooltip} arrow placement="top">
+            <a
+              href={"https://www.facebook.com/groups/1384367125377692"}
+              target="_blank"
+              className="linkFooter"
+            >
+              <BsFacebook />
+            </a>
+          </Tooltip>
+          <Tooltip title={instagramTooltip} arrow placement="top">
+            <a
+              href={"https://www.instagram.com/esncoruna/"}
+              target="_blank"
+              className="linkFooter"
+            >
+              <BsInstagram />
+            </a>
+          </Tooltip>
+          <Tooltip title={mapsTooltip} arrow placement="top">
+            <a
+              href={
+                "https://www.google.com/maps/place/Casa+do+Lagar+-+UDC/@43.332124,-8.412034,14z/data=!4m5!3m4!1s0x0:0xa2fa446abee0da28!8m2!3d43.3321242!4d-8.412034?hl=en"
+              }
+              target="_blank"
+              className="linkFooter"
+            >
+              <FiMapPin />
+            </a>
+          </Tooltip>
+          <Tooltip title={whatsappTooltip} arrow placement="top">
+            <a
+              // href={""}
+              target="_blank"
+              className="linkFooter"
+            >
+              <BsWhatsapp />
+            </a>
+          </Tooltip>
+        </div>
+        <div className="disclaimer">
+          &#169; ESN Coruña - &nbsp;
+          <Link to={"/privacy"} className="linkFooter">
+            Privacy
+          </Link>
+          &nbsp; - &nbsp;
+          <Link to={"/contact"} className="linkFooter">
+            Contact Us
+          </Link>
+        </div>
       </div>
       <div className="logos">
         <SlidingLogos />
--- esn/src/pages/Footer/SlidingLogos/SlidingLogos.js
+++ esn/src/pages/Footer/SlidingLogos/SlidingLogos.js
@@ -1,43 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SlidingLogos.css";
 import logo from "../../../assets/Images/logo.png";
 import logo1 from "../../../assets/Images/logo1.png";
+import { Tooltip, tooltipClasses } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 function SlidingLogos(props) {
+  const [onHover, setOnHover] = useState(false);
   let images = [
-    "../../../assets/Images/logo.png",
-    "../../../assets/Images/logo1.png",
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
+    logo,
+    logo1,
   ];
-  // for (let i = 0; i < 10; i + 2) {
-  //   images[i] = "../../../assets/Images/logo.png";
-  //   images[i + 1] = "../../../assets/Images/logo1.png";
-  // }
+
+  const HtmlTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      // backgroundColor: "#f5f5f9",
+      color: "rgba(0, 0, 0, 0.87)",
+      maxWidth: 220,
+      fontFamily: "Roboto Slab, sans-serif",
+      fontSize: "0.5rem",
+      border: "1px solid #dadde9",
+    },
+  }));
 
   return (
     <div className="static-container">
       {/* 2. */}
       <div className="animated-container">
         {/* 3 */}
-        <div className="img-container">
-          <img src="../../../assets/Images/logo.png" />
-        </div>
-        <div className="img-container">
-          <img src="../../../assets/Images/logo.png" />
-        </div>
-        <div className="img-container">
-          <img src="../../../assets/Images/logo.png" />
-        </div>
-        {/* {images.map((i) => {
-          return (
-          );
-        })} */}
-        {/* {images.map((i) => {
+        {images.map((i) => {
           return (
             <div className="img-container">
-              <img src={i} />
+              <HtmlTooltip
+                title={
+                  <React.Fragment>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexOrientation: "row",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img src={i} style={{ width: "50%" }} />
+                      <h1 style={{ marginLeft: "10px" }}>Migliori Panini</h1>
+                    </div>
+                  </React.Fragment>
+                }
+              >
+                <img
+                  src={i}
+                  style={{ width: "6.5vh" }}
+                  onMouseLeave={() => setOnHover(false)}
+                />
+              </HtmlTooltip>
             </div>
           );
-        })} */}
+        })}
+        {images.map((i) => {
+          return (
+            <div className="img-container">
+              <img src={i} style={{ width: "6.5vh" }} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
